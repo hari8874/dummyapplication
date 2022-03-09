aa402f207e955a8a93d2777c53898b34b3886f80
Hari sankar reddy Andloori <89625213+hari8874@users.noreply.github.com>
2022-03-09 23:01:20 +0530

    Update factorial.h

--- include/factorial.h
+++ include/factorial.h
@@ -1,4 +1,12 @@
 #ifndef INCLUDE_FACTORIAL_H_
 #define INCLUDE_FACTORIAL_H_
-int factorial(int n);
+int factorial(int n)
+{
+  int result=1;
+  for(int i=1;i<=n;i++)
+  {
+    result *=i;
+  }
+  return result;
+}
 #endif
